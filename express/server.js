3b0d5759dbee98db40da5773a2534d7c058085fd
noxlock <admin@noxlock.dev>
2020-04-14 20:45:12 +1000

    fixed the express route to static resources

--- express/server.js
+++ express/server.js
@@ -11,10 +11,10 @@ const rootRouter = require('./routes/root.js')
 app.use(rootRouter)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../react-src/build'))
+    app.use(express.static('../client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join('../client/build/index.html'))
+        res.sendFile('../client/build/index.html')
     })
 }
 
