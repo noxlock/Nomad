d2aac2f37458f4bc3ed04edf23f7c951b38fa2b1
noxlock <admin@noxlock.dev>
2020-04-14 20:21:54 +1000

    edited start scripts

--- express/server.js
+++ express/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../react-src/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'react-src', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
