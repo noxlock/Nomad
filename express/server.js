1196e056f6e64f236adb7b59b1c4508b7892ee95
noxlock <admin@noxlock.dev>
2020-04-14 20:14:03 +1000

    prep for deployment to heroku

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path')
 
 
 const app = express()
@@ -9,6 +10,13 @@ const port = process.env.PORT || 7000
 const rootRouter = require('./routes/root.js')
 app.use(rootRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../react-src/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'react-src', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`listening on https://127.0.0.1:${port}`)
